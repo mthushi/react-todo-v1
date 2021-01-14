07d9edda5a6e6f4b680ada2443b5bf07e2d8e718
Thushitha Manokaran <manothushitha@gmail.com>
2021-01-14 06:19:37 +0530

    added todo item, delete,edit icons

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -3,11 +3,21 @@ import Item from './TodoItem';
 
 class TodoList extends Component {
     render() {
+        const {items,clearList,handleDelete,handleEdit} = this.props;
         return (
-            <div>
-                <h1>Hello from todolist..</h1>
-                <Item></Item>
-            </div>
+            <ul className="List-group my-5">
+                <h3 className="text-capitalize text-center">todo list</h3>
+                {items.map(item => {return(
+                    <Item key={item.id} title={item.title} 
+                    handleDelete={()=>handleDelete(item.id)} 
+                    handleEdit={()=>handleEdit(item.id)}>
+                    </Item>
+                )}
+                )}
+                <button type="button" className="btn btn-block btn-danger text-uppercase-mt-5" onClick={clearList}>
+                    clear list
+                </button>
+            </ul>
         )
     }
 }
